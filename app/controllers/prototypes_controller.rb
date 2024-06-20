ebfa8fb87dab9c8fc856e8a8d5c2fb537c9b631b
SAKURAchan916 <syouto1443@gmail.com>
2024-06-20 16:51:46 +0900

    バリデーションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,21 @@
 class PrototypesController < ApplicationController
+
   def index
+    @prototype = prototype.all
   end
 
+  def new
+    @prototype = prototype.new
+  end
+
+  def create
+    prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
+  provete
+  def prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
