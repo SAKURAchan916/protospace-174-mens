b31e1230e5cfb7faeda066a097e4d561ed6a8881
SAKURAchan916 <syouto1443@gmail.com>
2024-06-20 15:52:28 +0900

    バリデーションの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
   def index
   end
+
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
