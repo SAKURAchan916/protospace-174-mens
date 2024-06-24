8be4c94fd7b6a7adab6b0af02380e128e9a45397
daisuke-w <w.daisuke.001@gmail.com>
2024-06-24 11:23:11 +0900

    rubocop修正対応

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,11 +1,11 @@
 class Prototype < ApplicationRecord
-    belongs_to :user
-    has_one_attached :image
+  belongs_to :user
+  has_one_attached :image
 
-    with_options presence: true do
-      validates :title
-      validates :catch_copy
-      validates :concept
-      validates :image
-    end
+  with_options presence: true do
+    validates :title
+    validates :catch_copy
+    validates :concept
+    validates :image
+  end
 end
